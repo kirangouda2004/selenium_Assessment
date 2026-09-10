76de32f0e15181b66307f63f00494dfa1a660de8
kirangouda2004 <167072567+kirangouda2004@users.noreply.github.com>
2026-09-10 10:15:40 +0530

    Update day3_2.java

--- day3/day3_2.java
+++ day3/day3_2.java
@@ -13,18 +13,16 @@ public class day3_2 {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://demoapps.qspiders.com/ui/datePick?sublist=0");
-		driver.findElement(By.cssSelector("[type='text']")).click();
-		driver.findElement(By.cssSelector("[type='button']")).click();
-		driver.findElement(By.cssSelector("[role='option']")).click();
-		WebElement result=  driver.findElement(By.cssSelector("[type='text']"));
+		driver.findElement(By.cssSelector("[type='text']")).click(); //click on calender
+		driver.findElement(By.cssSelector("[aria-label='Next Month']")).click(); //For selecting next month
+		driver.findElement(By.cssSelector("[aria-label='Choose Tuesday, October 6th, 2026']")).click(); //For selecting date
+		WebElement result=  driver.findElement(By.cssSelector("[type='text']"));  
 		result.getDomAttribute("value");
-		if(result.getDomAttribute("value").contains("26/07/2026")) {
+		if(result.getDomAttribute("value").contains("06/10/2026")) {
 			System.out.println("Date is displayed Correctly");
 			
 		}else {
 			System.out.println("Date is not displayed Correctly");
 		}
 	}
-
 }
-
